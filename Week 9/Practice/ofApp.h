32b77b47182b7d7bdb957e788954dc652faa6eb4
turkeyhan <hski0930@gmail.com>
2023-01-21 15:50:13 +0900

    Week 9
    Week 9

---
+++ Week 9/Practice/ofApp.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    /* WaterFall-related member variables Regions */
+    
+    // flag variables
+    int draw_flag;
+    int load_flag;
+    
+    // Line segment and dot related variables
+    int num_of_line, num_of_dot;
+    float dot_diameter;
+    
+    /* WaterFall-related member functions */
+    
+    void processOpenFileSelection(ofFileDialogResult openFileResult);
+    void initializeWaterLines(); // 2nd week portion.
+
+	typedef struct _Line
+	{
+		int x1;
+		int y1;
+		int x2;
+		int y2;
+	}Line;
+
+	typedef struct _Dot
+	{
+		int x1;
+		int y1;
+		bool operator < (const struct _Dot &tmp)
+		{
+			return x1 < tmp.x1;
+		}
+	}Dot;
+
+	Line *Lines;
+	Dot* Dots;
+	int cur;
+};
+
